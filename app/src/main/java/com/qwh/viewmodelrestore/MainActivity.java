452a0d44e9b8bad2aeddb6a7327eea2a2a75c046
qilanjie <qilanjie@126.com>
2020-06-03 14:40:34 +0800

    Signed-off-by: qilanjie <qilanjie@126.com>

---
+++ app/src/main/java/com/qwh/viewmodelrestore/MainActivity.java
@@ -0,0 +1,37 @@
+package com.qwh.viewmodelrestore;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.SavedStateViewModelFactory;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import com.qwh.viewmodelrestore.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    MyViewModel myViewModel;
+public final static  String KEY_NUMBER="my_number";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+        myViewModel=new ViewModelProvider(this).get(MyViewModel.class);
+//        if(savedInstanceState!=null){
+//            myViewModel.getNumber().setValue(savedInstanceState.getInt(KEY_NUMBER));
+//        }
+        binding.setData(myViewModel);
+        binding.setLifecycleOwner(this);
+
+    }
+
+//    @Override
+//    protected void onSaveInstanceState(@NonNull Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//        outState.putInt(KEY_NUMBER,myViewModel.getNumber().getValue());
+//
+//    }
+}
